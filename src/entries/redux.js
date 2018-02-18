ba7d8012cbe10dab7b418fefaa79dc24e9aeb4ae
willarevalo <willarevalo.developer@gmail.com>
2018-02-18 15:23:57 -0500

    1. Entendiendo y agregando un Store

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 // console.log("Ola k ase");
 // Esta funcion recoje lo de el form y lo manda a la consola
 function handleSubmit(event) {
@@ -11,3 +13,21 @@ function handleSubmit(event) {
 const $form = document.getElementById('form');
 // $form.addEventListener('evento', funcion);
 $form.addEventListener('submit', handleSubmit);
+
+const initialState = [
+	{
+		"title": "Despacito",
+	},
+	{
+		"title": "One more time",
+	},
+	{
+		"title": "Echame la culpa",
+	},
+]
+// Inicializo el store de redux
+const store = createStore(
+	(state) => state,
+	initialState,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
