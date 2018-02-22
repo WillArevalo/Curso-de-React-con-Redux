46ba38a70f708e7f83c31a9eafc267ce5431004c
willarevalo <willarevalo.developer@gmail.com>
2018-02-21 23:41:30 -0500

    18 - Filtrando los datos de búsqueda

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,12 +1,22 @@
 //Reducer y acciones
-const data = (state, action) => {
+function data (state, action) {
 	switch (action.type) {
 		case 'SEARCH_VIDEO': {
-			return state
+			const resultado = []
+	        state.data.categories.map(category => {
+	            category.playlist.map(item => {
+	                if(item.title.toLowerCase().includes(action.payload.query.toLowerCase()) || item.author.toLowerCase().includes(action.payload.query.toLowerCase())){
+	                    resultado.push(item)
+	                }
+	            })
+	        })
+	        return {
+	        	...state,
+	        	search:resultado
+	        }
 		}
-		default:{
+		default:
 			return state
-		}
 	}
 }
 export default data;
